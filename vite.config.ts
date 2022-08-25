1b474a3903a09d294284cc295c6940d8b67a442b
iBenita <Semporia@foxmail.com>
2022-08-25 13:40:19 +0800

    Math

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-/*
- * @Author: NMTuan
- * @Email: NMTuan@qq.com
- * @Date: 2022-06-21 19:41:22
- * @LastEditTime: 2022-06-27 11:36:46
- * @LastEditors: NMTuan
- * @Description:
- * @FilePath: \math_test_questions_generator\vite.config.ts
- */
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import Unocss from 'unocss/vite'
